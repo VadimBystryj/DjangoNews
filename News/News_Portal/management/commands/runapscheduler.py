70e96afbed7bc99526c072b1bbd1041f37190032
Vadzim <vadimbystryj@gmail.com>
2023-07-21 20:41:50 +0200

    Update runapscheduler.py

--- News/News_Portal/management/commands/runapscheduler.py
+++ News/News_Portal/management/commands/runapscheduler.py
@@ -55,7 +55,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(),
+            trigger=CronTrigger(day_of_week="fri", hour="18", minute="00"),
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
